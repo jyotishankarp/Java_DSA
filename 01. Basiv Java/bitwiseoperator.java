87426cc17d705ce747ae78da544eae12537c596d
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-08-20 23:48:31 +0530

    bitwise operator

--- 01. Basiv Java/bitwiseoperator.java
+++ 01. Basiv Java/bitwiseoperator.java
@@ -1,5 +1,33 @@
+
+// & | ~ ^(Bitwise Operator)
+import java.util.Scanner;
+
 public class bitwiseoperator {
-    public static void main(String[] args){
-    System.out.println("sdbvjasdhbvkjadb");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+        System.out.print("& Operator:");
+        System.out.println(2 & 3);
+        System.out.print("OR Operator:");
+        System.out.println(2 | 3);
+        System.out.print("negation/complement Operator:");
+        System.out.println(~3);
+        System.out.print("XOR Operator:");
+        System.out.println(2 ^ 3);
+
+        System.out.print("Left Shift Operator");
+        System.out.println(34<<1);
+        // System.out.println(-4<<1);
+
+        System.out.print("Left Shift Operator");
+        System.out.println(34>>2);
+        // System.out.println(-4>>1);
+
+        // bitwise01(num);
+        sc.close();
     }
+
+    static void bitwise01(int num) {
+
+    };
 }
