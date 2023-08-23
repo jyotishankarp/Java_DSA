062d5c48bc49727cbc5306155f015e74b8d24af9
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-08-23 23:59:15 +0530

    XD

--- 01. Basiv Java/8.Lecture 5. Function, Variables and their Scope/Functions02.java
+++ 01. Basiv Java/8.Lecture 5. Function, Variables and their Scope/Functions02.java
@@ -1,5 +1,11 @@
 
 public class Functions02 {
+    public static boolean Prime(int n){
+        int num = 
+        for (int )
+        return false;
+        return true;
+    }
     public static void main(String[] args){
     System.out.println();
     }
