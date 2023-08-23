97945f42b995056819cf4e771cadeed4ecce639b
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-08-24 00:07:18 +0530

    prime  number

--- 01. Basiv Java/8.Lecture 5. Function, Variables and their Scope/Functions02.java
+++ 01. Basiv Java/8.Lecture 5. Function, Variables and their Scope/Functions02.java
@@ -1,12 +1,17 @@
 
 public class Functions02 {
-    public static boolean Prime(int n){
-        int num = 
-        for (int )
-        return false;
+    public static boolean Prime(int n) {
+        for (int i = 2; i < (n / 2); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
         return true;
+
     }
-    public static void main(String[] args){
-    System.out.println();
+
+    public static void main(String[] args) {
+        boolean ans = Prime(25);
+        System.out.println(ans);
     }
 }
