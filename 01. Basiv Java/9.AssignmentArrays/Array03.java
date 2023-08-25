15fc167d624a3d616719bcc8225363de6aa50310
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-08-25 23:52:11 +0530

    array input method

---
+++ 01. Basiv Java/9.AssignmentArrays/Array03.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Array03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        // int [2,5,4,8,7,9,6,4,2,5,1]
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        sc.close();
+    }
+}
