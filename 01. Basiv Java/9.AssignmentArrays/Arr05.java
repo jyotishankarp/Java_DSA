e6c56982ee2f40928a76dfe8d55981c5f2962938
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-08-26 17:23:04 +0530

    Array referance of an array

---
+++ 01. Basiv Java/9.AssignmentArrays/Arr05.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Arr05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10];
+        System.out.println(arr); // [I@2c7b84de
+        System.out.println(sc); //sc is an referance
+    }
+
+}
