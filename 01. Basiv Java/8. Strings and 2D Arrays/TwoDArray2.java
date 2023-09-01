7250144304750d6c3a4ec6560e099404963fca1f
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-09-01 23:30:20 +0530

    2nd

---
+++ 01. Basiv Java/8. Strings and 2D Arrays/TwoDArray2.java
@@ -0,0 +1,8 @@
+public class TwoDArray2 {
+    public static void main(String[] args) {
+        // Another Way to create array
+        // int arr2D[][] = new arr2D[2][2];
+        int arr2[][] = { { 1, 2 }, { 3, 4 } };
+        System.out.println();
+    }
+}
