cb4ee0e72444e6d586918e557ff2d986210ba244
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-09-05 23:09:57 +0530

    recursion problem 01

---
+++ 01. Basiv Java/12.Recursion/Recursion01.java
@@ -0,0 +1,16 @@
+public class Recursion01 {
+    public static int fact(int n) {
+        // will give error so we need basecase
+        if (n == 0) {
+            return 1;
+        }
+        int smallAns = fact(n - 1);
+        return n * smallAns;
+    }
+
+    public static void main(String[] args) {
+        int n = 13;
+        int ans = fact(n);
+        System.out.println(ans);
+    }
+}
