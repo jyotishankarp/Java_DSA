bbab8435af6cb47fd573299645a3bf09cb03678c
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-09-11 00:36:45 +0530

    rec problem solved

--- 04. DSA/DSA 2/Recursion 02/Rec01.java
+++ 04. DSA/DSA 2/Recursion 02/Rec01.java
@@ -1,6 +1,19 @@
 // Q: I have an string with multiple character and i have to remove the given character from the string ?
 public class Rec01 {
-    public static void main(String[] args){
-    System.out.println();
+    public static String RemoveX(String str) {
+        if (str.length() == 0) {
+            return "";
+        }
+        String ans = "";
+        if (str.charAt(0) != 'x') {
+            ans += str.charAt(0);
+        }
+        String smallAns = RemoveX(str.substring(1));
+        return ans + smallAns;
+    }
+
+    public static void main(String[] args) {
+        String str = "xabdxzjx";
+        System.out.println(RemoveX(str));
     }
 }
