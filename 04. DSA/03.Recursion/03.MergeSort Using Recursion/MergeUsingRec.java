aae63d7e2e6a4f2cf2d47560bdf2f5736c8d10b9
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-09-12 23:39:01 +0530

    merge sort rec revise

--- 04. DSA/03.Recursion/03.MergeSort Using Recursion/MergeUsingRec.java
+++ 04. DSA/03.Recursion/03.MergeSort Using Recursion/MergeUsingRec.java
@@ -14,10 +14,38 @@ public class MergeUsingRec {
 
     private static void merge(int input[], int SI, int EI) {
         int mid = (SI + EI) / 2;
-        int ans = new int[]
+        int ans[] = new int[EI - SI + 1];
+        int i = SI;
+        int j = mid + 1;
+        int k = 0;
+        while (i <= mid && j <= EI) {
+            if (input[i] < input[j]) {
+                ans[k] = input[i];
+                // ans[i] = input[j]
+                i++;
+                k++;
+            } else {
+                ans[k] = input[j];
+                k++;
+                j++;
+            }
+        }
+        while (i <= mid) {
+            ans[k] = input[i];
+            i++;
+            k++;
+        }
+        while (j <= EI) {
+            ans[k] = input[j];
+            k++;
+            j++;
+        }
     }
 
     public static void main(String[] args) {
+        int arr[] = { 1, 65, 65, 465, 46, 65, 46, 6, 465, 46, 54, 654, 65, 32, 132, 3, 3, 1313, 323, 6, 46, 6, 46 };
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println(arr);
         System.out.println();
     }
 }
