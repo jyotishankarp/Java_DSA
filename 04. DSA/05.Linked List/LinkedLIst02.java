214a2f0bf0298de37c43fe1805883d9efb91a5b8
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-09-22 09:17:14 +0530

    LinkedList(print function)

--- 04. DSA/05.Linked List/LinkedLIst02.java
+++ 04. DSA/05.Linked List/LinkedLIst02.java
@@ -1,5 +1,15 @@
 
 public class LinkedLIst02 {
+
+    public static void Print(Node<Integer> head) {
+        System.out.println();
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+
+    }
+
     public static void main(String[] args) {
         Node<Integer> node1 = new Node<Integer>(10);
         Node<Integer> node2 = new Node<Integer>(20);
@@ -9,6 +19,7 @@ public class LinkedLIst02 {
         Node<Integer> head = node1;
 
         System.out.print(head.data + " ");
+        Print(head);
         System.out.println();
 
         while (head != null) {
