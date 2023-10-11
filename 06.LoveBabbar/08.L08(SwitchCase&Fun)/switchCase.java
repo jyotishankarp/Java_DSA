e4ab22690aef1e50281f2c2b0252402158e1b8a2
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-10-11 23:58:47 +0530

    minor fun

--- 06.LoveBabbar/08.L08(SwitchCase&Fun)/switchCase.java
+++ 06.LoveBabbar/08.L08(SwitchCase&Fun)/switchCase.java
@@ -1,7 +1,13 @@
 public class switchCase {
+    static int SumNum(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
         int num = 5;
-        switchCase:
-        System.out.println("Hello");
+        int num2 = 7;
+        int ans = SumNum(num, num2);
+        System.out.println("The sum of " + num + " and " + num2 + " is " + ans);
+        // switchCase: System.out.println("Hello");
     }
 }
