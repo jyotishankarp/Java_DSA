dc803bd36c9e134f66c48ade7a49d1053f9cb49f
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2023-12-23 23:24:31 +0530

    find unique

---
+++ 06.LoveBabbar/09.L09(Arrays)/Problems/FindUnique.java
@@ -0,0 +1,19 @@
+
+public class FindUnique {
+    static int traverseArray(int arr[],int l,int n){
+        for(int i = 0;i<l;i++){
+            if()
+        }
+        return null;
+    }
+    static int FindUniq(int arr[],int l){
+        for(int i = 0;i<l;i++){
+            if(arr[i])
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = { 2, 3, 1, 6, 3, 6, 2 };
+        int l = 7;
+        System.out.println();
+    }
+}
