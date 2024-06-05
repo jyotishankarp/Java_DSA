69a8c894d07fa06a9c09758432ae9d487f4b16cd
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2024-06-05 23:43:17 +0530

    pointers

---
+++ 06.LoveBabbar/L25/Pointer.java
@@ -0,0 +1,11 @@
+public class Pointer {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(num);
+
+        //address of operator
+        System.out.println(&num);
+        System.out.println();
+        Integer number = 5;
+    }
+}
