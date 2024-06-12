daccd781a3b28d21cb53d0dcd97f975572492561
Jyotishankar Panda <jyotishankarpanda55@gmail.com>
2024-06-12 22:33:33 +0530

    Sum of array using recursion

---
+++ 06.LoveBabbar/L32Day3/SumOfArray.java
@@ -0,0 +1,30 @@
+public class SumOfArray {
+
+    public static int sumArray(int arr[], int size) {
+        if (size <= 0)
+            return 0;
+        if (size == 1)
+            return arr[0]; // no need of this just writing
+
+        int[] newArr = new int[size - 1];
+        System.arraycopy(arr, 1, newArr, 0, size - 1);
+
+        int sum = 0;
+        sum = sum + arr[0];
+        return sum + sumArray(newArr, size - 1);
+    }
+
+    // Better Approach anc clean code
+    public int sumArray2(int arr[], int size) {
+        if (size <= 0)
+            return 0;
+        if (size == 1)
+            return arr[0];
+        return arr[size - 1] + sumArray(arr, size - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 5, 6, 7, 6 };
+        System.out.println(sumArray(arr, arr.length));
+    }
+}
